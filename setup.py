e5a056deffa31ef107a085fdf01ac89de50c1390
Geoff Ford <g_ford@hotmail.com>
2015-07-01 21:40:19 +1000

    Convert MD to reST for pypi

--- setup.py
+++ setup.py
@@ -2,14 +2,23 @@
 
 from setuptools import setup
 
-setup(name='xml_models2',
-      version='0.7.0',
-      description='XML backed models queried from external REST apis',
-      author='Geoff Ford and Chris Tarttelin and Cam McHugh',
-      author_email='g_ford@hotmail.ccom',
-      url='http://github.com/alephnullplex/xml_models',
-      packages=['xml_models'],
-      install_requires=['lxml', 'python-dateutil', 'pytz', 'future', 'requests'],
-      tests_require=['mock', 'nose', 'coverage'],
-      test_suite="nose.collector"
-      )
+try:
+    import pypandoc
+
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name='xml_models2',
+    version='0.7.0',
+    description='XML backed models queried from external REST apis',
+    long_description=long_description,
+    author='Geoff Ford and Chris Tarttelin and Cam McHugh',
+    author_email='g_ford@hotmail.ccom',
+    url='http://github.com/alephnullplex/xml_models',
+    packages=['xml_models'],
+    install_requires=['lxml', 'python-dateutil', 'pytz', 'future', 'requests'],
+    tests_require=['mock', 'nose', 'coverage'],
+    test_suite="nose.collector"
+)
