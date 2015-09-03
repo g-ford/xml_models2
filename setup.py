377ffd9c7c1284f4e5e615ab27b73c55e8e2c5c8
Geoff Ford <g_ford@hotmail.com>
2015-09-03 14:24:10 +1000

    Bump version: 0.8.0 → 0.9.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except(IOError, ImportError):
 
 setup(
     name='xml_models2',
-    version='0.8.0',
+    version='0.9.0',
     description='XML backed models queried from external REST apis',
     long_description=long_description,
     author='Geoff Ford and Chris Tarttelin and Cam McHugh',
